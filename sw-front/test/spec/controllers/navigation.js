e0eb9c23a7e64a6a717abfc919eb4563a348a447
gultaj <yaschick.mail@gmail.com>
2014-11-04 14:54:32 +0300

    e2e test. navigation test

---
+++ sw-front/test/spec/controllers/navigation.js
@@ -0,0 +1,29 @@
+'use strict';
+
+describe('Controller: MainCtrl', function () {
+
+  var location, scope, controller;
+
+  beforeEach(module('swFrontApp'));
+
+  beforeEach(inject(function($controller, $rootScope, $location) {
+    location = $location;
+    scope = $rootScope.$new();
+    controller = $controller('NavigationController', {$scope: scope});
+  }));
+
+  describe('isActive', function() {
+    it('return true when path are the same', function() {
+      location.path('/test');
+      expect(scope.isActive('/test')).toBeTruthy();
+    });
+    it('return false when path are different', function() {
+      location.path('/diff');
+      expect(scope.isActive('/test')).toBeFalsy();
+    });
+    it('return true when path are the same', function() {
+      location.path('/test?query');
+      expect(scope.isActive('/test')).toBeTruthy();
+    });
+  });
+});
